d7fc63b882dd663c9f8e2d063efbbb27102af440
mariano22 <marianocrosetti1993@gmail.com>
2016-04-02 23:20:51 -0300

    globalfiles + localfiles

--- erlang/globalfiles.erl
+++ erlang/globalfiles.erl
@@ -1,6 +1,40 @@
 -module(globalfiles).
 -compile(export_all).
 
+% DEBUG
+dbg([]) -> io:format("~n~n");
+dbg([X|L]) -> io:format(lists:concat(["{",element(1,X),",",element(2,X),"} "])),
+              dbg(L).
 
-archivosActuales () -> ["caca", "pito"]. % TODO
-getOwner(NameFile) -> error("TODO5874165"). % noOwner  | WId
+loop( L ) ->
+    %dbg(L),
+    receive
+        {P, archivosActuales}  -> { Archivos , _ } = lists:unzip( L ),
+                                  P ! Archivos ,
+                                  loop( L ) ;
+        {P, getOwner, NameFile} -> case lists:keyfind(NameFile,1,L) of
+                                        false -> P ! noOwner ;
+                                        WId   -> P ! element(2,WId)
+                                   end,
+                                   loop( L ) ;
+        {P, baja, NameFile}     -> Lp = lists:keydelete(NameFile,1,L),
+                                   P ! ok,
+                                   loop( Lp ) ;
+        {P, alta, NameFile, WId}   -> Lp = [{NameFile,WId}|L],
+                                   P ! ok,
+                                   loop( Lp ) ;
+        _ -> error("esto no deberia suceder 666") 
+    end.
+
+setUp() -> register( globalfilesserver, spawn(?MODULE,loop,[[]]) ).
+
+archivosActuales () -> globalfilesserver ! {self(), archivosActuales }, 
+                       receive X -> X end.
+getOwner(NameFile)  -> globalfilesserver ! {self(), getOwner, NameFile }, 
+                       receive X -> X end.
+baja(NameFile) ->  globalfilesserver ! {self(), baja, NameFile }, 
+                   receive X -> X end.
+alta(NameFile,WId) ->  globalfilesserver ! {self(), alta, NameFile, WId }, 
+                       receive X -> X end.
+
+
--- erlang/localfiles.erl
+++ erlang/localfiles.erl
@@ -2,5 +2,62 @@
 -compile(export_all).
 
 
-status( file ) -> error("Indefinido").
-delete( file ) -> error("Indefinido").
+% DEBUG
+dbg([]) -> io:format("~n~n");
+dbg([X|L]) -> io:format(lists:concat(["{",element(1,X),",",element(2,X),",",element(3,X),"} "])),
+              dbg(L).
+
+loop( L ) ->
+    %dbg(L),
+    receive
+        {P, status, NameFile} -> case lists:keyfind(NameFile,1,L) of
+                                      false -> P ! noFile ;
+                                      T -> if element(2,T)>0 -> P ! reading ;
+                                              element(3,T)>0 -> P ! writting ;
+                                              true           -> P ! unused
+                                           end
+                                  end,
+                                  loop( L ) ;
+        {P, delete, NameFile}     -> Lp = lists:keydelete(NameFile,1,L),
+                                   P ! ok,
+                                   loop( Lp ) ;
+        {P, openR, NameFile}     -> Lb = lists:keydelete(NameFile,1,L),
+                                    T = lists:keyfind(NameFile,1,L),
+                                    Lp = [ {element(1,T),element(2,T)+1,element(3,T)} | Lb ],
+                                    P ! ok,
+                                    loop( Lp ) ;
+        {P, openW, NameFile}     -> Lb = lists:keydelete(NameFile,1,L),
+                                    T = lists:keyfind(NameFile,1,L),
+                                    Lp = [ {element(1,T),element(2,T),element(3,T)+1} | Lb ],
+                                    P ! ok,
+                                    loop( Lp ) ;
+        {P, close, NameFile }   -> Lb = lists:keydelete(NameFile,1,L),
+                                   T = lists:keyfind(NameFile,1,L),
+                                   Tp= if element(2,T)>0 -> {element(1,T),element(2,T)-1,element(3,T)};
+                                          element(3,T)>0 -> {element(1,T),element(2,T),element(3,T)-1};
+                                          true -> error("No deberia suceder") end,
+                                   Lp = [ Tp | Lb ],
+                                   P ! ok,
+                                   loop( Lp ) ;
+        {P, create, NameFile }   -> Lp = [{NameFile,0,0}|L],
+                                   P ! ok,
+                                   loop( Lp ) ;
+        _ -> error("esto no deberia suceder 666") 
+    end.
+
+setUp() -> register( localfilesserver, spawn(?MODULE,loop,[[]]) ).
+
+status( NameFile ) -> localfilesserver ! {self(), status , NameFile }, 
+                  receive X -> X end.
+delete( NameFile ) -> localfilesserver ! {self(), delete , NameFile }, 
+                  receive X -> X end.
+openR( NameFile ) -> localfilesserver ! {self(), openR , NameFile }, 
+                  receive X -> X end.
+openW( NameFile ) -> localfilesserver ! {self(), openW , NameFile }, 
+                  receive X -> X end.
+create( NameFile ) -> localfilesserver ! {self(), create , NameFile }, 
+                  receive X -> X end.
+close( NameFile ) -> localfilesserver ! {self(), close , NameFile }, 
+                  receive X -> X end.
+
+
