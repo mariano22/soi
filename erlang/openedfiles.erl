e07cf58c73c475a6a6aff5351b5626e2a478d56a
mariano22 <marianocrosetti1993@gmail.com>
2016-04-03 00:49:38 -0300

    openedfiles

---
+++ erlang/openedfiles.erl
@@ -0,0 +1,43 @@
+-module(openedfiles).
+-compile(export_all).
+
+% La funcion de este modulo es llevar un seguimiento de los GlobalFd tomados por clientes conectados a este worker
+% Funciones
+% registerOpen : GlobalFd, ClientId -> ok (Registra que el GlobalFd fue tomado por el ClientId)
+% registerClose : GlobalFd -> ok (Elimina a GlobalFd de la tabla, para cuando el cliente cierra el archivo)
+% globalFdList : ClientId -> [GlobalFd] (Lista los GlobalFd que actualmente tiene en posecion el cliente, muy util a la hora de hacer un bye)
+
+
+% DEBUG
+dbg([]) -> io:format("~n~n");
+dbg([X|L]) -> io:format(lists:concat(["{",element(1,X),",",element(2,X),"} "])),
+              dbg(L).
+
+loop( L ) ->
+    %dbg(L),
+    receive
+        {P, globalFdList , ClientId }    -> Rp = lists:filter(fun(X) -> element(2,X)==ClientId end,L),
+                                            R = lists:map(fun(X) -> element(1,X) end,Rp),
+                                            P ! R ,
+                                            loop( L ) ;                                    
+        {P,  registerClose , GlobalFd }  -> Lp = lists:keydelete(GlobalFd,1,L),
+                                            P ! ok,
+                                            loop( Lp ) ;
+        {P, registerOpen, GlobalFd, ClientId }   -> Lp = [{GlobalFd,ClientId}|L],
+                                   P ! ok,
+                                   loop( Lp ) ;
+        _ -> error("esto no deberia suceder 666") 
+    end.
+
+setUp() -> register( openedfilesserver, spawn(?MODULE,loop,[[]]) ).
+
+registerOpen( GlobalFd, ClientId ) -> openedfilesserver ! {self(), registerOpen , GlobalFd, ClientId }, 
+                  receive X -> X end.
+registerClose( GlobalFd ) -> openedfilesserver ! {self(), registerClose , GlobalFd }, 
+                  receive X -> X end.
+globalFdList( ClientId ) -> openedfilesserver ! {self(), globalFdList , ClientId }, 
+                  receive X -> X end.
+
+
+
+
