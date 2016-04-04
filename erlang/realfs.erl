64329515d83e03d95a561a01f25a2ac70eb1df3c
mariano22 <marianocrosetti1993@gmail.com>
2016-04-04 17:32:49 -0300

    realfs

--- erlang/realfs.erl
+++ erlang/realfs.erl
@@ -1,7 +1,31 @@
 -module(realfs).
 -compile(export_all).
+-define(WORKER_PATH,"./").
 
-openr( NameFile) -> error("No implementado").
-openw( NameFile) -> error("No implementado").
-read ( Handle, Size) -> error("No implementado").
-write ( Handle, Txt) -> error("No implementado").
+% este modulo es el encargado de encapsular la interaccion con el sistema de archivos real subyacente
+% Funciones
+% openr : String -> Handle (abre un archivo en modo lectura)
+% openw : String -> Handle (abre un archivo en modo escritura)
+% read : Handle, Size -> Data | eof (trata de leer hasta Size bytes del archivo, si puede leer algo devuelve los caracteres leidos, si el archivo ya terminó devuelve eof)
+% write : Handle, String -> ok (escribe String en el archivo)
+% create : String -> ok (crea un archivo)
+% delete : String -> ok (crea un archivo existente)
+
+
+
+openr( NameFile) -> {ok, Handle} = file:open(?WORKER_PATH ++ NameFile, [read]),
+                    Handle.
+openw( NameFile) -> {ok, Handle} = file:open(?WORKER_PATH ++ NameFile, [append]),
+                    Handle.
+read ( Handle, Size) -> case file:read(Handle,Size) of
+                            eof -> eof;
+                            {ok,Data} -> Data;
+                            _ -> error("No deberia ocurrir")
+                        end.
+write ( Handle, Data) -> file:write(Handle, Data),
+                            ok.
+close ( Handle ) -> file:close( Handle ), ok.
+
+create( NameFile ) -> file:write_file(?WORKER_PATH ++ NameFile,""), ok.
+
+delete( NameFile ) -> file:delete(?WORKER_PATH ++ NameFile), ok.
