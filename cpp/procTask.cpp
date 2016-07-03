6e3c0d14858e3f22697e83e029820de46443dc41
Lauta <lautarorinaldi92@gmail.com>
2016-07-03 17:40:35 -0300

    actualizando y subiendo moddificaciones

--- cpp/procTask.cpp
+++ cpp/procTask.cpp
@@ -1,13 +1,19 @@
 #include <bits/stdc++.h>
+#include <string>
+#include "ids.h"
+#include "task.h"
 #include "comunic.h"
 #include "mensaje.h"
+#include "globalFiles.h"
+#include "tokenQueues.h"
 
 using namespace std;
+#define forall(it,v) for(typeof(v.begin()) it=v.begin();it!=v.end();++it)
+
 #define dprint(v) cerr << #v"=" << v << endl //;)
 #define forr(i,a,b) for(int i=(a); i<(b); i++)
 #define forn(i,n) forr(i,0,n)
 #define dforn(i,n) for(int i=n-1; i>=0; i--)
-#define forall(it,v) for(auto it=v.begin();it!=v.end();++it)
 #define sz(c) ((int)c.size())
 #define zero(v) memset(v, 0, sizeof(v))
 #define pb push_back
@@ -19,23 +25,53 @@ typedef pair<int,int> ii;
 const int MAXN=100100;
 int n;
 
-enum taskName { userLsd, userDelete, userCreate, userOpenRead, userOpenWrite, userWrite, userRead, userClose, userBye, userCon, 
-			    workerDelete, workerOpenRead, workerWrite, workerRead, workerOpenWrite, workerSay, workerOpenSucc, workerClose, 
-				workerCloseBye, workerCloseSucc, workerToken } ;
+void caseUserCon(WorkerScope *who,task& t){
+				ClientId c = t.getCliente();
+				responderCliente(c,mensaje::coneccionEstablecida(c),who);
+}
 
-struct task {
-	taskName getTaskName() const {return userLsd;}
-};
+void caseUserLsd(WorkerScope *who,task& t){
+			globalFiles gl = globalFiles();
+			mensaje l = mensaje::mOk();
+			vector<string> arch = gl.archivosActuales();
+			forall (it, arch ){ l.addArg(*it);  };
+			ClientId cID = t.getCliente();
+			responderCliente(cID,l, who);
+}
 
-void procTask(WorkerScope *who,const task& t) {
+void caseUserCreate(WorkerScope *who,task& t){
+	string name = t.getFileName();
+	ClientId cID = t.getCliente();
+	WorkerId wID = who->MyglobalFiles.getOwner(name);
+	if (noWorkerId!=wID){
+		responderCliente(cID,mensaje::archivoExistente(),who);
+	}else{
+		if (who->MytokenQueues.isInCreate(name)) {
+			responderCliente(cID,mensaje::archivoExistente(),who);
+		}else{
+			who->MytokenQueues.newCreate(name, cID);
+		};
+	}; 
+}
+void caseUserDelete(WorkerScope *who,task& t){
+	int a=3;
+	
+}
+	
+	
+void procTask(WorkerScope *who,task& t) {
 	switch (t.getTaskName()) {
-		case userLsd:
-				ClientId c=getCliente().t;
-				responderCliente(c,coneccionEstablecida(c));
+		case userCon:
+			caseUserCon(who,t); 
 		break;
-		case userDelete:
+		case userLsd:
+			caseUserLsd(who,t);
 		break;
 		case userCreate:
+			caseUserCreate(who,t);
+		break;
+		case userDelete:
+			caseUserDelete(who,t);
 		break;
 		case userOpenRead:
 		break;
@@ -48,8 +84,6 @@ void procTask(WorkerScope *who,const task& t) {
 		case userClose:
 		break;
 		case userBye:
-		break;
-		case userCon:
 		break;
 		case workerDelete:
 		break;
